f071e30d02be7fec9c2b47be7dce68a7127f9d12
Marianne Anderson <mganderson13@gmail.com>
2025-06-19 18:44:55 -0500

    script logs deal based on dummy data set

--- scripts/content.js
+++ scripts/content.js
@@ -5,3 +5,14 @@ const baseUrl = window.location.origin;
 console.log("baseURL", baseUrl);
 const hostnameUrl = window.location.hostname;
 console.log("hostnameURL", hostnameUrl);
+
+const data = [
+  "www.gap.com",
+  "www.anntaylor.com",
+  "www.google.com",
+  "www.freecodecamp.org"
+]
+
+if(data.includes(hostnameUrl)){
+  console.log("🚨!!DEAL!!🚨");
+}
