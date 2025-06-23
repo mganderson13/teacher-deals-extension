b950a0d8fe281ae00dedcf32bc08360b9ba30876
Marianne Anderson <mganderson13@gmail.com>
2025-06-23 13:32:16 -0500

    getData function and forEach to check for deal

--- scripts/content.js
+++ scripts/content.js
@@ -7,15 +7,25 @@ console.log(loggerLabel, "baseURL", baseUrl);
 const hostnameUrl = window.location.hostname;
 console.log(loggerLabel, "hostnameURL", hostnameUrl);
 
-const data = [
-  "www.gap.com",
-  "www.anntaylor.com",
-  "www.google.com",
-  "www.freecodecamp.org"
-]
+let siteData = [];
 
-if(data.includes(hostnameUrl)){
-  console.log(loggerLabel, "🚨!!DEAL!!🚨");
-}else {
-  console.log(loggerLabel, "No deal found");
+async function getData() {
+  const jsonURL = chrome.runtime.getURL("data/deals.json");
+  try {
+    const response = await fetch(jsonURL);
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+    siteData = await response.json(); // assign to outer variable
+  } catch (err) {
+    console.error("Error loading JSON:", err.message);
+  }
 }
+
+getData().then(() => {
+  siteData.forEach((brand)=> {
+if (hostnameUrl.toLowerCase().includes(brand.Brand.toLowerCase())) {
+    console.log(loggerLabel, "🚨!!DEAL!!🚨");
+    return;
+    }
+  })
+});
+
